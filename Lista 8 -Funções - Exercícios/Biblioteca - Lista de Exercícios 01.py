ab0bf4134f8828f2d488bd40198254a22e2008a3
Lucas Antonio da Silva <30278242+niubajr19@users.noreply.github.com>
2017-10-12 22:48:47 -0300

    Add files via upload

---
+++ Lista 8 -Funções - Exercícios/Biblioteca - Lista de Exercícios 01.py
@@ -0,0 +1,27 @@
+
+def testaVogal(a):
+    if a == 'a' or a == 'e' or a == 'i' or a == 'o' or a == 'u':
+        res = 'True'
+    else:
+        res = 'False
+    return res
+
+def calculaMaior(a,b):
+    if a > b :
+        return a
+    if b > a:
+        return b
+
+def testaMultiplo4(a):
+    if a % 4 == 0:
+        return 'True'
+    else:
+        return 'False'
+
+def TestaDivisor(a,b):
+    if a % b == 0:
+        return 'True'
+    else:
+        return 'False'
+
+
